9cb5cc69e6da06d72c295cfc32d9416c8c6ea898
Alexander <abkolodiychuk@gmail.com>
2017-06-22 22:22:14 +0200

    Launching ball on drag

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
 
 public class Ball : MonoBehaviour {
-
-  public Vector3 InitialVelocity = new Vector3(0.0f, -0.3f, 700.0f);
   
+  public static readonly float DefaultYVelocity = -0.3f;
+
+  public Vector3 InitialVelocity = new Vector3(0.0f, DefaultYVelocity, 700.0f);
+
   private Rigidbody _rigidbody;
   private AudioSource _audioSource;
-
 
   private void Start() {
     _rigidbody = GetComponent<Rigidbody>();
     _audioSource = GetComponent<AudioSource>();
-
-    _rigidbody.velocity = InitialVelocity;
-    _audioSource.Play();
+    _rigidbody.useGravity = false;
+    // Launch(InitialVelocity);
   }
 
+  public void Launch(Vector3 velocity) {
+    _rigidbody.useGravity = true;
+    _rigidbody.velocity = velocity;
+    _audioSource.Play();
+  }
+  
 }
---
+++ Assets/Scripts/TouchHandler.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class TouchHandler : MonoBehaviour {
+
+  private Ball _ball;
+  private Vector2 _position;
+
+
+  private void Start() {
+    _ball = FindObjectOfType<Ball>();
+  }
+
+  public void OnDragStart(BaseEventData eventData) {
+    var pointerData = eventData as PointerEventData;
+    if (pointerData != null) {
+      _position = pointerData.position;
+    }
+    // remember position
+  }
+
+  public void OnDragEnd(BaseEventData eventData) {
+    var pointerData = eventData as PointerEventData;
+    if (pointerData != null) {
+      var velocity = _position - pointerData.position;
+      _ball.Launch(new Vector3(-1 * velocity.x, Ball.DefaultYVelocity, -1 * velocity.y));
+    }
+  }
+
+}
