9edaa58500cb6d50dbe956351d0ce5702e86c1d5
Alexander <abkolodiychuk@gmail.com>
2017-06-22 22:47:46 +0200

    Taking time into account when calculating launch velocity, tolerance of x offset

--- Assets/Scripts/TouchHandler.cs
+++ Assets/Scripts/TouchHandler.cs
@@ -4,7 +4,9 @@ using UnityEngine.EventSystems;
 public class TouchHandler : MonoBehaviour {
 
   private Ball _ball;
-  private Vector2 _position;
+  private Vector2 _dragStartPosition;
+  private float _dragStartTime;
+  private const float XTolerance = 0.5f;
 
 
   private void Start() {
@@ -14,17 +16,29 @@ public class TouchHandler : MonoBehaviour {
   public void OnDragStart(BaseEventData eventData) {
     var pointerData = eventData as PointerEventData;
     if (pointerData != null) {
-      _position = pointerData.position;
+      _dragStartPosition = pointerData.position;
+      _dragStartTime = Time.timeSinceLevelLoad;
     }
-    // remember position
   }
 
   public void OnDragEnd(BaseEventData eventData) {
     var pointerData = eventData as PointerEventData;
     if (pointerData != null) {
-      var velocity = _position - pointerData.position;
-      _ball.Launch(new Vector3(-1 * velocity.x, Ball.DefaultYVelocity, -1 * velocity.y));
+      var dragEndPosition = pointerData.position;
+      var dragEndTime = Time.timeSinceLevelLoad;
+      var dragDuration = dragEndTime - _dragStartTime;
+      var dragVector = _dragStartPosition - dragEndPosition;
+      var translatedVector = new Vector3(-dragVector.x, Ball.DefaultYVelocity, -dragVector.y);
+      _ball.Launch(LimitedVelocity(translatedVector / dragDuration));
     }
+  }
+
+  private static Vector3 LimitedVelocity(Vector3 velocity) {
+    return new Vector3(
+      XTolerance * velocity.x,
+      velocity.y,
+      Mathf.Clamp(velocity.z, 0, 1000)
+    );
   }
 
 }
