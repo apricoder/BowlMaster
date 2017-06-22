ff3d2b021c1009b614b1f16ffd5d4bd07a810109
Alexander <abkolodiychuk@gmail.com>
2017-06-22 08:39:20 +0200

    Added Ball, small refactor of pins

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+  public int InitialSpeed = 700;
+  
+  private Rigidbody _rigidbody;
+  private AudioSource _audioSource;
+
+
+  private void Start() {
+    _rigidbody = GetComponent<Rigidbody>();
+    _audioSource = GetComponent<AudioSource>();
+
+    _rigidbody.velocity = new Vector3(0.0f, -0.3f, 1.0f) * InitialSpeed;
+    _audioSource.Play();
+  }
+
+}
