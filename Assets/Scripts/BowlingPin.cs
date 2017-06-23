d34a0d25373fc5c0580ab660afa06fd70fcaf65b
Alexander <abkolodiychuk@gmail.com>
2017-06-23 21:54:35 +0200

    Counting if pin is standing

---
+++ Assets/Scripts/BowlingPin.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BowlingPin : MonoBehaviour {
+
+  public float StandingThreshold = 3.0f;
+
+  public bool IsStanding() {
+    return
+      transform.rotation.eulerAngles.x <= StandingThreshold &&
+      transform.rotation.eulerAngles.z <= StandingThreshold;
+  }
+
+}
