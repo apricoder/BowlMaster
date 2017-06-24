fa70d6491dcea68d24d8d8c4de57aadc15698ba1
Alexander <abkolodiychuk@gmail.com>
2017-06-24 09:14:39 +0200

    Setting score text to green after 3 seconds of not changing count of standing pins

--- Assets/Scripts/PlayBox.cs
+++ Assets/Scripts/PlayBox.cs
@@ -7,7 +7,9 @@ public class PlayBox : MonoBehaviour {
 	private BowlingPin[] _pins;
 	private PinsCountText _standingPinsCountText;
 	private int _standingPinsCount;
+	private float _standingPinsCountChangeTime;
 	private bool _countingScore;
+	private const float ChangeTimeTolerance = 3.0f;
 
 	private void Start() {
 		_pins = FindObjectsOfType<BowlingPin>();
@@ -15,10 +17,25 @@ public class PlayBox : MonoBehaviour {
 	}
 
 	private void Update() {
-		if (_countingScore) {
-			_standingPinsCount = _pins.Count(p => p && p.IsStanding());
-			_standingPinsCountText.GetComponent<Text>().text = _standingPinsCount.ToString();
+		var standingCount = _pins.Count(p => p && p.IsStanding());
+		if (_countingScore && _standingPinsCount != standingCount) {
+			UpdateStandingPinsCount(standingCount);
 		}
+
+		var timeSinceLastChange = Time.timeSinceLevelLoad - _standingPinsCountChangeTime;
+		if (_countingScore && timeSinceLastChange > ChangeTimeTolerance) {
+			PinsHaveSettled();
+		}
+	}
+
+	private void UpdateStandingPinsCount(int standingCount) {
+		_standingPinsCount = standingCount;
+		_standingPinsCountText.GetComponent<Text>().text = _standingPinsCount.ToString();
+		_standingPinsCountChangeTime = Time.timeSinceLevelLoad;
+	}
+
+	private void PinsHaveSettled() {
+		_standingPinsCountText.GetComponent<Text>().color = Color.green;
 	}
 
 	private void OnTriggerEnter(Collider other) {
