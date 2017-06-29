d952e5fecc66c963c826f52e21e7fee02ecaa7c7
Alexander <abkolodiychuk@gmail.com>
2017-06-29 09:01:30 +0200

    First useful bowling score test

---
+++ Assets/Editor/Tests/ActionMasterTest.cs
@@ -0,0 +1,21 @@
+﻿using NUnit.Framework;
+
+namespace Tests {
+
+  [TestFixture]
+  public class ActionMasterTest {
+
+    private readonly ActionMaster _actionMaster = new ActionMaster();
+
+    [Test]
+    public void T00_SimplestTest() {
+      Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T01_StrikeGivesEndTurn() {
+      Assert.AreEqual(ActionMaster.Action.EndTurn, _actionMaster.Bowl(10));
+    }
+
+  }
+}
---
+++ Assets/Scripts/ActionMaster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class ActionMaster {
+
+  public enum Action {
+
+    Tidy,
+    EndTurn,
+    EndGame
+
+  }
+
+  public Action Bowl(int pins) {
+    if (pins < 0 || pins > 10) {
+      throw new UnityException("Invalid pin count!");
+    }
+
+    if (pins == 10) {
+      return Action.EndTurn;
+    }
+
+    throw new UnityException("Unhandled pin count!");
+  }
+
+}
